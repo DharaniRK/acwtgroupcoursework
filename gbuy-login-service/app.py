d1a2d04b0bd02bd4bc89a91202892ee1f4cf6d38
Dharani Rayadurgam <dr00546@surrey.ac.uk>
2022-04-22 16:09:27 +0100

    Added database connection code for login service

--- gbuy-login-service/app.py
+++ gbuy-login-service/app.py
@@ -1,18 +1,21 @@
 from flask import Flask,request
 import json
+import pymysql
 
 
 app= Flask(__name__)
+db = pymysql.connect('localhost','root','password','gbuy')
+cursor= db.cursor()
 
 @app.route("/login", methods=['POST'])
 
 def get_userdetails():
+
     data=json.loads(request.data.decode())
     print(data)
-    username=data["username"]
+    email=data["email"]
     password=data["password"]
-
-    get_userpassword="dharani"
+    get_userpassword=cursor.execute("SELECT password FROM users WHERE EmailID = '%s'" % (email))
     #select password from user_details where username=username
     if password==get_userpassword:
         print('true')
